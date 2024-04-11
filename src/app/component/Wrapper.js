b368857926446c6270e2116c552cfd966ded8ef2
Roshan Chouhan <roshanchouhan29@gmail.com>
2024-04-12 00:42:07 +0530

    test

--- src/app/component/Wrapper.js
+++ src/app/component/Wrapper.js
@@ -6,6 +6,7 @@ const Wrapper = ({ children, className }) => {
              className={`w-full max-w-[1280px] px-5 md:px-10 mx-auto ${
                 className || " "}`}
         >
+         
           {children}
         </div>
     );
